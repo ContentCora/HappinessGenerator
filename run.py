e832a7c29cfa2b169689ceeddc3e4e7141cd22f7
Cora Egli <learner-70cc96@extensionschool.ch>
2020-07-02 16:39:16 +0200

    added title and description as placeholder values

--- run.py
+++ run.py
@@ -7,14 +7,17 @@ app = Flask(__name__)
 @app.route("/")
 def start():
     
-    return render_template("start.html")
+    return render_template("start.html", title="Welcome to the Happiness Generator", 
+                                            metadescription="This app lets you look for Happiness Items and add to the list yourself.")
 
 @app.route("/random")
 def random():
     
-    return render_template("random.html")
+    return render_template("random.html", title="Your randomly selected Happiness Item", 
+                                            metadescription="This Happiness Item was randomly selected from a list. I hope it brings you joy.")
 
 @app.route("/list")
 def entire_list():
 
-    return render_template("list.html")
+    return render_template("list.html", title="All Happiness Items, united",
+                                            metadescription="This is the complete list of Happiness Items. You can add to the list or edit existing items. Enjoy!")
