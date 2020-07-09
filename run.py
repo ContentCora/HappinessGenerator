25992e4350d59ffe3a23173a8afdf83b09e129ef
Cora Egli <learner-70cc96@extensionschool.ch>
2020-07-09 15:28:38 +0200

    function get_hi() leads to error on hi/list

--- run.py
+++ run.py
@@ -3,21 +3,28 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Here comes the function to open the list of happiness items
+def get_hi(): #short for "get happiness items"
+    hi_db = open("happiness_items.txt") #hi_db is short for "happiness items database"
+    content = hi_db.read()
+    hi_db.close()
+    # put the values of content into an array. "hitems" is short for "happiness items"
+    hitems = content.split("\n")
+    return hitems
 
 @app.route("/")
 def start():
     
     return render_template("start.html", title="Welcome to the Happiness Generator", 
-                                            metadescription="This app lets you look for Happiness Items and add to the list yourself.")
+                                            metadescription="This app lets you look for Happiness Items and add to the list yourself. This Happiness Item was randomly selected from a list. I hope it brings you joy.", 
+                                            HAPPINESSITEM="Hello there!")
 
-@app.route("/random")
-def random():
-    
-    return render_template("random.html", title="Your randomly selected Happiness Item", 
-                                            metadescription="This Happiness Item was randomly selected from a list. I hope it brings you joy.")
-
-@app.route("/list")
+@app.route("/hi_list")
 def entire_list():
-
+    hi_list = get_hi()
+    hi_values = "" # hi_values = happiness item values
+    for item in hitems:
+        #put each HI (hitems) into one separate paragraph
+       hi_values += "<p>" + hitems + "<p>"
     return render_template("list.html", title="All Happiness Items, united",
-                                            metadescription="This is the complete list of Happiness Items. You can add to the list or edit existing items. Enjoy!")
+                                            metadescription="This is the complete list of Happiness Items. You can add to the list or edit existing items. Enjoy!", 
+                                            happiness="Hi! Here's your list")                               
