bf7cb28803148f87af38f9738b36f3926b59822a
Cora Egli <learner-70cc96@extensionschool.ch>
2020-09-17 10:07:15 +0200

    comments added for clarity. CSS class 'box' now applied to p tag instead of div.

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# Here comes the function to open the list of happiness items
+# This is the function to open the list of happiness items
 def get_hi(): #short for "get happiness items"
     hi_db = open("happiness_items.txt") #hi_db is short for "happiness items database"
     content = hi_db.read()
@@ -21,11 +21,6 @@ def start():
 @app.route("/list")
 def entire_list():
     hi_list = get_hi()
-    # hi_values = "" # hi_values = happiness item values
-    # for item in hi_list:
-    #     #put each HI (hi_list) into one separate paragraph
-    #     hi_values += item + "\n"
-    #     hi_values += "<p>" + item + "</p>"
-    # print(hi_values)
+    # The placeholder {{happiness}} in list.html is being replaced by the individual Happiness Items, all in a <p> tag with the class "box".
     return render_template("list.html", title="All Happiness Items, united", metadescription="This is the complete list of Happiness Items. You can add to the list or edit existing items. Enjoy!", happiness=hi_list)
                                                                     
